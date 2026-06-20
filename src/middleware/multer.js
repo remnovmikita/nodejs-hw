711bdb94d13190c0293163c9c65a923d350689d1
Nikita <remnovmikita@gmail.com>
2026-06-20 19:52:09 +0300

    fix 1

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -8,7 +8,7 @@ export const upload = multer({
     fileSize: 2 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-    if (file.mimetype.startWith("image/")) {
+    if (file.mimetype.startsWith("image/")) {
       cb(null, true);
     } else {
       cb(
